0913de739fa02b325034b863941a35eac6b03e33
Kerko0 <eric.kjelldorff@telia.com>
2022-09-07 11:58:25 +0200

    100% done!

--- Labb1/Program.cs
+++ Labb1/Program.cs
@@ -1,74 +1,100 @@
 ﻿using System;
 using System.Data;
 
-string input = "29535123p48723487597645723645";
-bool endIndexSet = false;
-bool pathHasLetter = false;
-bool isEntireStringIndexed = false;
-int startIndex = 0;
-int endIndex = 0;
-
-int lineCount = 0;
-while (!isEntireStringIndexed)
+public class Program
 {
-    LineIndexer();
-    LinePrinter(startIndex, endIndex);
-    pathHasLetter = false;
-    endIndexSet = false;
-    lineCount++;
-}
-
-void LineIndexer()
-{
-    startIndex = lineCount;
-    char nextNumToCheck = input[lineCount];
-    for (int letterCount = 0; letterCount < input.Length; letterCount++)
+    static void Main()
     {
-        if (nextNumToCheck == input[letterCount])
+        Labb1.Labb1Program();
+    }
+}
+public class Labb1
+{ 
+    static bool endIndexSet = false;
+    static bool pathHasLetter = false;
+    static bool isEntireStringIndexed = false;
+    static int startIndex = 0;
+    static int endIndex = 0;
+    static int lineCount = 0;
+    static string input;
+    static Int64 allNumbersAdded = 0;
+
+    public static void Labb1Program()
+    {
+        Console.Write("Please input a random string of letters and numbers: ");
+        input = Console.ReadLine();
+
+        while (!isEntireStringIndexed)
         {
-            
+            LineIndexer();
+            LinePrinter(startIndex, endIndex);
+            pathHasLetter = false;
+            endIndexSet = false;
+            lineCount++;
+        }
+
+        Console.ResetColor();
+        Console.WriteLine("Everything marked in red added together:");
+        Console.WriteLine(allNumbersAdded);
+
+    }
+    public static void LineIndexer()
+    {
+        if (lineCount == (input.Length - 1))
+        {
+            isEntireStringIndexed = true;        
+        }
+
+        startIndex = lineCount;  
+        char nextNumToCheck = input[lineCount];  
+        for (int letterCount = 0; letterCount < input.Length; letterCount++)
+        {
+            if (nextNumToCheck == input[letterCount])
+            {         
                 if (letterCount > startIndex && !endIndexSet)
                 {
-                    endIndexSet = true;
-                    endIndex = letterCount + 1;
+                   endIndexSet = true;
+                   endIndex = letterCount + 1;
                 }          
+            }
+        }
+
+        for(int i = startIndex; i < endIndex; i++)
+        {
+            if (char.IsLetter(input[i]))
+            {
+                pathHasLetter = true;
+            }                 
         }
     }
 
-    for(int i = 0 + startIndex; i < endIndex; i++)
+    public static void LinePrinter(int startIndex, int endIndex)
     {
-        if (char.IsLetter(input[i]))
+        string fullNumber = "";
+                
+        if (!pathHasLetter && endIndexSet)
         {
-            pathHasLetter = true;
-        }                 
-    }
-
-    if(endIndex == input.Length)
-    {
-        isEntireStringIndexed = true;
-    }
-}
-
-void LinePrinter(int startIndex, int endIndex)
-{
-    if (!pathHasLetter)
-    {
-        for (int i = 0; i < input.Length; i++)
-        {
-            if(i >= startIndex && i < endIndex)
+            for (int i = 0; i < input.Length; i++)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.White;
+                if(i >= startIndex && i < endIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    fullNumber = fullNumber + input[i];
+                }
+                else
+                {
+                    Console.ResetColor();
+                }
+
+                Console.Write(input[i]);
+                
             }
 
-            Console.Write(input[i]);
-
+            allNumbersAdded = allNumbersAdded + Convert.ToInt64(fullNumber);
+            Console.WriteLine();
         }
-
-        Console.WriteLine();
     }
 }
+
+
 
